35ae52e3d56131be00cb44900862d29452abe3cf
xuhexin <xuhxin@yonyou.com>
2024-01-18 14:01:42 +0800

    fix: 第十七节：对页面的增删改查接口进行适配(依赖于服务端的代码)

--- src/pages/builder/designTop/index.jsx
+++ src/pages/builder/designTop/index.jsx
@@ -1,9 +1,29 @@
+import { Button, message } from 'antd'
 import './index.css'
+import Store from '../../../store'
+import axios from 'axios'
 
 export default function DesignTop() {
+
+  const savePage = () => {
+    const search = window.location.search || '';
+    const pageId = search.replace('?pageId=', '');
+    const comList = Store.getState().comList;
+    axios.post('http://localhost:4000/page-json/updatePage', {
+      pageId,
+      pageJson: comList
+    })
+    .then(res => {
+      if(res.data.code == 200) {
+        message.success('保存成功')
+      }
+    })
+  }
+
   return (
     <div className='designTop'>
       <span className='title'>XinBuilder</span>
+      <Button onClick={savePage} type='primary' ghost>保存</Button>
     </div>
   )
 }
--- src/pages/builder/index.tsx
+++ src/pages/builder/index.tsx
@@ -1,9 +1,29 @@
+import { useEffect } from 'react'
 import DesignTop from './designTop'
 import LeftCom from './leftPart'
 import MainCom from './mainPart'
 import RightCom from './rightPart'
+import axios from 'axios'
+import Store from '../../store'
+import { message } from 'antd'
 
 export default function Builder() {
+
+  useEffect(() => {
+    const search = window.location.search || '';
+    const pageId = search.replace('?pageId=', '');
+    axios.post('http://localhost:4000/page-json/findPageByID', {
+      pageId
+    })
+    .then(res => {
+      if(res.data.data) {
+        Store.dispatch({type: 'changeComList', value: res.data.data.pageJson || []})
+      }else{
+        message.error('获取页面详情失败')
+      }
+    })
+  }, [])
+
   return (
     <div>
       <DesignTop />
